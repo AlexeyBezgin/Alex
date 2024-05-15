fe95388e4b9cf8b618d5de0b92710028aa8fa0e3
Bezgin Alexey <bezginaa@ingradproekt.ru>
2024-05-15 16:05:11 +0300

    Revert "Добавил наполнение тестовому модулю"
    This reverts commit 8642c3e978c4c76e90571d6962ebf6ea8a1a8fdd.

--- my_cosiak_my_rules.py
+++ my_cosiak_my_rules.py
@@ -1,6 +1,9 @@
+<<<<<<< HEAD
 def pick_rooms():
     references = uidoc.Selection.PickObjects(
         Selection.ObjectType.Element, RoomSelectionFilter(),
         'Выделите помещения')
     return [doc.GetElement(reference)
 for reference in references]
+=======
+>>>>>>> parent of 8642c3e (Добавил наполнение тестовому модулю)
